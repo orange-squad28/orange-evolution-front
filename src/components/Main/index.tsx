4fef7f2ecf0250bfc1fe682d7157916a0e46ccf4
Rodrigo Alexandre <rodrigo.anst@gmail.com>
2022-11-01 22:21:52 -0300

    Clonado o repositorio da organizacao

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -9,7 +9,7 @@ export default function Main() {
 
       <S.Logo src="/img/logo_day.png" alt="Imagem de um átomo e React Avançado escrito ao lado." />
 
-      <S.Title> Boilerplate Next.JS </S.Title>
+      <S.Title> Este é um titulo </S.Title>
 
       <S.Description>
         Este é um projeto simples para iniciar um projeto com Next.JS com TypeScript, Styled Components para uso na criação de interfaces e aplicativos web.
