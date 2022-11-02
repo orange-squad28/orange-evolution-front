e1400a2a7c2ed4972b4a1305faf18fc29ed9feff
Rodrigo Alexandre <rodrigo.anst@gmail.com>
2022-11-01 23:33:29 -0300

    Refatorado o código para uso do CSS Modules

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,24 +1,27 @@
-import * as S from './styles'
-
-
-
+import style from './Main.module.css'
 
 export default function Main() {
   return (
-    <S.Wrapper>
+    <main className={style.wrapper}>
+      <img
+        className={style.logo}
+        src="/img/logo_day.png"
+        alt="Imagem de um átomo e React Avançado escrito ao lado."
+      />
 
-      <S.Logo src="/img/logo_day.png" alt="Imagem de um átomo e React Avançado escrito ao lado." />
+      <h1>Este é um titulo</h1>
 
-      <S.Title> Este é um titulo </S.Title>
+      <h2>
+        Este é um projeto simples para iniciar um projeto com Next.JS com
+        TypeScript, Styled Components para uso na criação de interfaces e
+        aplicativos web.
+      </h2>
 
-      <S.Description>
-        Este é um projeto simples para iniciar um projeto com Next.JS com TypeScript, Styled Components para uso na criação de interfaces e aplicativos web.
-
-      </S.Description>
-
-      <S.Illustration src="/img/illustration.svg" alt="Uma montanha como caminho para a web" />
-
-
-    </S.Wrapper>
+      <img
+        className={style.ilustration}
+        src="/img/illustration.svg"
+        alt="Uma montanha como caminho para a web"
+      />
+    </main>
   )
 }
