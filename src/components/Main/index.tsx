1640be1c063a4cdeeec59ee6da65512883c11bba
Charles Eduardo Mello Guimaraes <charles.eduardo.mello.guimaraes@hotmail.com>
2022-11-04 20:03:13 -0300

    Carousel

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,34 +1,17 @@
 import Cabecalho from '../Cabecalho'
 import style from './Main.module.css'
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 
+import Carousel from '../Carousel';
 
 export default function Main() {
   return (
-
     <>
      <Cabecalho/>
-     
-    <main className={style.wrapper}>
-    <img
-      className={style.logo}
-      src="/img/orangeLogo.png"
-      alt="Logo de uma laranja 'derretendo'."
-    />
+      <Carousel/>
+            
+       
+      </>
+        );
   
-  <div className={style.containerHome}>
-      <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</h1>
-
-
-      <h2>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-      </h2>
-
-      <div className={style.ilustration}>
-      </div>
-  </div>
-    </main>
-    </>
-   
-    
-  )
 }
