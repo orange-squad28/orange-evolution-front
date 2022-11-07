0aa7b315845bb424b69a06882d0f936d5606c4b6
Amanda Freitas <amandaafreitas20@gmail.com>
2022-11-07 16:05:17 -0300

    alterando font family para Montserrat

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,10 +1,14 @@
 import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
+
+@import url('https://fonts.googleapis.com/css2?family=Gemunu+Libre:wght@200;300&family=Montserrat:wght@800&display=swap');
+
 * {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
+  font-family: 'Montserrat', sans-serif;
 }
 
 html {
@@ -15,8 +19,6 @@ html, body, #__next {
   height: 100%;
 }
 
-body{
-  font-family: --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-}
+
 `
 export default GlobalStyle
