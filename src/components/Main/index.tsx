5838b5d6213c20e2ffd72dd567c41033179a698f
narradorww <rodrigo.anst@gmail.com>
2022-11-08 16:27:56 -0300

    Refatorado o componente Cabecalho

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -7,28 +7,12 @@ export default function Main() {
 
     <>
      <Cabecalho/>
-     
-    <main className={style.wrapper}>
-    <img
-      className={style.logo}
-      src="/img/orangeLogo.png"
-      alt="Logo de uma laranja 'derretendo'."
-    />
-  
-  <div className={style.containerHome}>
-      <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</h1>
+
+    <main className={style.wrapper}/>
 
 
-      <h2>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-      </h2>
-
-      <div className={style.ilustration}>
-      </div>
-  </div>
-    </main>
     </>
-   
-    
+
+
   )
 }
