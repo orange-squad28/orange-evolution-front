6db21338552e103b38d0b88e84dcca3b3e4452d6
narradorww <rodrigo.anst@gmail.com>
2022-11-11 21:52:54 -0300

    teste de conexao com a api

---
+++ src/pages/trilhas.tsx
@@ -0,0 +1,12 @@
+import Cabecalho from "src/components/Cabecalho";
+import TrilhasdeUmAluno from "src/components/Trilhas";
+
+
+export default function Trilhas() {
+  return (
+    <>
+      <Cabecalho />
+      <TrilhasdeUmAluno id={"636c74ccef85974d2f943113"}/>
+    </>
+  );
+}
