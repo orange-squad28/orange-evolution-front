92f312752f208be34f527b7006d0cf058fea4878
Amanda Freitas <amandaafreitas20@gmail.com>
2022-11-12 19:25:35 -0300

    criacao de modal para inserir informação de cardo do admin

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -26,22 +26,4 @@ const Input = forwardRef((props: InputProps, ref) => {
   )
 })
 
-// const Input = forwardRef<HTMLInputElement, InputProps>(
-//   ({ tipo, texto, icone }: InputProps, ref) => {
-//     return (
-//       <>
-//         <div className={style.container}>
-//           <img className={style.icone} src={`/img/${icone}-icon.svg`} alt="" />
-//           <input
-//             className={style.input}
-//             type={tipo}
-//             placeholder={texto}
-//             ref={ref}
-//           />
-//         </div>
-//       </>
-//     )
-//   }
-// )
-
 export default Input
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -3,49 +3,86 @@ import Cabecalho from 'src/components/Cabecalho'
 import Input from 'src/components/Input'
 import style from './Login.module.css'
 import { useForm, SubmitHandler } from 'react-hook-form'
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import axios from 'axios'
 import api from 'src/services/api'
+import Modal from 'react-modal'
+import ModalCadastroAdmin from 'src/components/Modal-cadastro-admin'
 
+Modal.setAppElement('#__next')
 type Inputs = {
   nome: string
   email: string
   senha: string
 }
 
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    backgroundColor: '#0c1828',
+    borderRadius: '20px',
+  },
+}
+
 export default function Login() {
+  const [modalIsOpen, setIsOpen] = useState(false)
+  const [dadosInputs, setDadosInputs] = useState({})
+  const [cargoInput, setCargoInput] = useState('')
+
+  function openModal() {
+    setIsOpen(true)
+  }
+
+  function closeModal() {
+    setIsOpen(false)
+  }
+
+  function finalizacaoCadastroAdmin() {
+    const todosOsDados = {
+      ...dadosInputs,
+      cargo: cargoInput,
+    }
+
+    api({
+      method: 'post',
+      url: '/admins',
+      data: todosOsDados,
+    })
+      .then((response) => closeModal())
+      .catch((error) => console.error(error))
+  }
+
+  function preencherCargo(e) {
+    setCargoInput(e.target.value)
+  }
+
   const {
     register,
     handleSubmit,
     watch,
     formState: { errors },
   } = useForm<Inputs>({})
+
   const onSubmit: SubmitHandler<Inputs> = (valorInputs, evento) => {
-    // api({ url: '/alunos' }).then(({ data }) => console.log(data))
-    // api({
-    //   method: 'post',
-    //   url: '/alunos',
-    //   data: valorInputs,
-    // })
-    //   .then((response) => console.log(response))
-    //   .catch((error) => console.error(error))
-    console.log(evento)
-
-    const reqBody = {
-      method: 'post',
-      url: '',
-      data: valorInputs,
-    }
-
     const botaoId = evento?.nativeEvent?.submitter?.id
+
     if (botaoId === 'admin') {
-      reqBody.url = '/admins'
+      setDadosInputs(valorInputs)
+      openModal()
     } else {
-      reqBody.url = '/alunos'
+      api({
+        method: 'post',
+        url: '/alunos',
+        data: valorInputs,
+      })
+        .then((response) => console.log(response))
+        .catch((error) => console.error(error))
     }
-    api(reqBody)
-      .then((response) => console.log(response))
-      .catch((error) => console.error(error))
   }
 
   return (
@@ -61,7 +98,12 @@ export default function Login() {
               </p>
             </div>
 
-            <BotaoGrande texto="Entrar" cor="#01132B" id="login" />
+            <BotaoGrande
+              texto="Entrar"
+              cor="#01132B"
+              id="login"
+              tipo="button"
+            />
           </aside>
           <form onSubmit={handleSubmit(onSubmit)} className={style.form_login}>
             <h1 className={style.form_login_titulo}>
@@ -127,9 +169,53 @@ export default function Login() {
             </div>
 
             <div className={style.form_botoes}>
-              <BotaoGrande texto="Ensinar" cor="#2B73BF" id="admin" />
-              <BotaoGrande texto="Aprender" cor="#4A49C7" id="aluno" />
+              <BotaoGrande
+                texto="Ensinar"
+                cor="#2B73BF"
+                id="admin"
+                tipo="submit"
+              />
+              <BotaoGrande
+                texto="Aprender"
+                cor="#4A49C7"
+                id="aluno"
+                tipo="submit"
+              />
             </div>
+
+            <Modal
+              isOpen={modalIsOpen}
+              onRequestClose={closeModal}
+              style={customStyles}
+              contentLabel="Example Modal"
+            >
+              <div className={style.modal_container}>
+                <h2 className={style.modal_titulo}>
+                  Olá, admin! Digite seu cargo.
+                </h2>
+                <div className={style.cargo}>
+                  <label className={style.cargo_texto} htmlFor="cargo">
+                    Cargo:
+                  </label>
+                  <input
+                    className={style.input}
+                    type="text"
+                    id="cargo"
+                    onChange={preencherCargo}
+                  />
+                </div>
+                <a title="Fechar" className={style.fechar} onClick={closeModal}>
+                  x
+                </a>
+
+                <button
+                  className={style.botao}
+                  onClick={finalizacaoCadastroAdmin}
+                >
+                  Ok
+                </button>
+              </div>
+            </Modal>
           </form>
         </div>
       </div>
