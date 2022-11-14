96bd7b89781fa5e5a4c2aabeb5344ac90bd0cf8d
Eduardo Mello <102185386+EduardoMG12@users.noreply.github.com>
2022-11-14 17:49:05 -0300

    Trilha "home" dos usuários (#20)
    * add: new page trilhasHome
    
    * update: responsive

--- src/components/Cursos/index.tsx
+++ src/components/Cursos/index.tsx
@@ -1,8 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import api from '../../services/api';
 
+interface ICurso {
+  id: string,
+  titulo: string,
+  video: string,
+  descricao: string,
+  imagem: string,
+  descritivoDaImagem: string,
+  duracao: string,
+  dataCriacao: string,
+  autor: string,
+  estaAtivo: boolean,
+  dataAtualização: string,
+}
+
 export default function Cursos() {
-  const [cursos, setCursos] = useState([]);
+  const [cursos, setCursos] = useState<ICurso[]>([]);
 
   useEffect(() => {
     api
@@ -11,26 +25,28 @@ export default function Cursos() {
       .catch((error) => console.log(`ocorre um erro: ${error}`));
 
   }, []);
+  useEffect(() => {
+    api
+      .get('/admins')
+      .then((response) => setCursos(response.data))
+      .catch((error) => console.log(`ocorre um erro: ${error}`));
 
-  return (
-    <>
-      <h1>Cursos</h1>
+  }, []);
 
-      {cursos.map((curso) => (
-        <>
-          <p key={curso?.id}>{curso?.titulo}</p>
-          <p>{curso.autor}</p>
-          <p>{curso?.id}</p>
-          <img src={curso?.imagem} alt={curso?.titulo} />
-          (estaAtivo ? <p>Ativo</p> : <p>Inativo</p>)
-          <p>{curso.dataCriacao}</p>
-          <p>{curso.dataAtualização}</p>
-          <p>{`${curso.duracao} minutos`}</p>
-          <p>{curso?.descricao}</p>
-          <video src={curso?.video} controls></video>
-        </>
-
-      ))}
-    </>
-  );
+  return cursos.map((curso) => {
+    return (
+      <div key={curso?.id}>
+        <p>{curso?.titulo}</p>
+        <p>{curso.autor}</p>
+        <p>{curso?.id}</p>
+        <img src={curso?.imagem} alt={curso?.titulo} />
+        (estaAtivo ? <p>Ativo</p> : <p>Inativo</p>)
+        <p> {curso.dataCriacao}</p>
+        <p>{curso.dataAtualização}</p>
+        <p>{`${curso.duracao} minutos`}</p>
+        <p>{curso?.descricao}</p>
+        <video src={curso?.video} controls></video>
+      </div>
+    );
+  })
 }
