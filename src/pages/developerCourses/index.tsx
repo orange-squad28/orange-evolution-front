5a03a53159dc13e953c76ce63d96d7f66a9a8c5a
Charles Eduardo Mello Guimaraes <charles.eduardo.mello.guimaraes@hotmail.com>
2022-11-15 00:13:33 -0300

    update: add progress bar, points and emblem in user trials

--- src/pages/developerCourses/index.tsx
+++ src/pages/developerCourses/index.tsx
@@ -10,7 +10,23 @@ export default function index() {
         <>
             <Cabecalho />
             <div className={style.container}>
-            <InfoTrilha admin={false} tempo={'100'} tituloTrilha={"full stack developer"} guias="Charles Edurado, Maria Amanda, Jade Figueiredo"/>
+                <InfoTrilha admin={false} tempo={'100'} tituloTrilha={"full stack developer"} guias="Charles Edurado, Maria Amanda, Jade Figueiredo" />
+                <h2 className={style.tittle}>SEU PROGRESSO NESTA TRILHA</h2>
+                <div className={style.containerProgressEmblem}>
+                    <div className={style.progress}>
+                        <div className={style.progressAllBar}></div>
+                        <div className={style.progressBar}></div>
+                    </div>
+                    <div className={style.containerEmblem}>
+                        <div className={style.containerPoints}>
+                            <div className={style.points}></div>
+                            <div className={style.points}>
+                            </div>
+                        </div>
+                        <div className={style.emblem}><div className={style.star}></div></div>
+                        <div className={style.emblem}><div className={style.star}></div></div>
+                    </div>
+                </div>
                 <div className={style.containerCourses}>
                     <h2 className={style.tittle}>O INÍCIO</h2>
                     <div className={style.containerCourse}>
@@ -25,7 +41,7 @@ export default function index() {
                         {Courses()}
                     </div>
                 </div>
-          
+
             </div>
         </>
     )
