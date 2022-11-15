6ae7470704ec772573f886d405ddb177a867ec5e
narradorww <rodrigo.anst@gmail.com>
2022-11-15 07:42:19 -0300

    adicionado o contexto de autenticacao

---
+++ src/provider/Context.tsx
@@ -0,0 +1,49 @@
+import React, { Children, createContext, ReactNode, useContext, useState } from "react";
+
+
+type authContextType = {
+  user: boolean | null;
+  login: () => void;
+  logout: () => void;
+};
+
+const authContextDefaultValues: authContextType = {
+  user: null,
+  login: () => {},
+  logout: () => {},
+};
+
+const AuthContext = createContext<authContextType>(authContextDefaultValues);
+
+export function useAuth() {
+    return useContext(AuthContext);
+}
+
+type Props = {
+  children: ReactNode;
+};
+
+export function AuthProvider({ children }: Props) {
+  const [user, setUser] = useState<boolean>();
+  const login = () => {
+    setUser(true);
+};
+
+const logout = () => {
+    setUser(false);
+};
+
+const value = {
+  user,
+  login,
+  logout,
+};
+
+  return (
+      <>
+          <AuthContext.Provider value={value}>
+              {children}
+          </AuthContext.Provider>
+      </>
+  );
+}
